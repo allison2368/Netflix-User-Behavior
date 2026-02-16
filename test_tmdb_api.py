ecee89e64657f87f4bbf22c54e409873b382e6fb
Saeah Go <saeah.go@gmail.com>
2026-02-15 16:07:12 -0800

    Remove hardcoded API key and use environment variable

--- test_tmdb_api.py
+++ test_tmdb_api.py
@@ -4,7 +4,10 @@ import json
 from time import sleep
 import os
 
+<<<<<<< HEAD
 	
+=======
+>>>>>>> 221205e (Remove hardcoded API key and use environment variable)
 API_KEY = os.getenv("API_KEY")
 
 # movie_name = "Squid Game"
